6c4a6884a255ea254c3b61e5885fbb63b1c09c88
Ruan Lucas <79365912+RuanLucasGD@users.noreply.github.com>
2021-04-17 11:50:53 -0300

    impl: add  itens/ show data

---
+++ Source/List.cs
@@ -0,0 +1,83 @@
+using System;
+
+namespace CSharp_Custom_List
+{
+    public class List
+    {
+        private class Node
+        {
+            private Node next;
+            private Node last;
+
+            private float value;
+
+            public Node(float v)
+            {
+                this.value = v;
+            }
+
+            public Node Next { get => next; set => next = value; }
+            public Node Last { get => Last; set => Last = value; }
+            public float Value { get => value; set => this.value = value; }
+        }
+
+        private int size;
+
+        private Node head;
+
+        /// <summary>
+        /// Adiciona um novo valor no final da lista
+        /// </summary>
+        /// <param name="v">
+        /// novo valor
+        /// </param>
+        public void Add(float v)
+        {
+            var newNode = new Node(v);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                var current = head;
+
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = newNode;
+            }
+
+            size++;
+        }
+
+        /// <summary>
+        /// Imprime todos os dados da lista
+        /// </summary>
+        public void ShowData()
+        {
+            if (head == null)
+            {
+                return;
+            }
+
+            var current = head;
+
+            while (current != null)
+            {
+                Console.WriteLine(current.Value);
+                current = current.Next;
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <value>
+        /// Tamanho da lista
+        /// </value>
+        public int Size { get => size; }
+    }
+}
---
+++ Source/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CSharp_Custom_List;
+
+namespace CSharp_Custom_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List myList = new List();
+
+            myList.Add(5);
+            myList.Add(3);
+            myList.Add(1);
+            myList.Add(7);
+            myList.Add(3);
+            myList.ShowData();
+
+            Console.WriteLine(myList.Size);
+
+            Console.ReadKey();
+        }
+    }
+}
